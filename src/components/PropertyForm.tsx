146f49a402fd493e283c3a87774d50b32acf7f71
sharon-chepkoech <66520912+sharon-chepkoech@users.noreply.github.com>
2025-10-07 12:18:49 +1100

    Update PropertyForm.tsx

--- src/components/PropertyForm.tsx
+++ src/components/PropertyForm.tsx
@@ -92,7 +92,7 @@ const PropertyForm: React.FC = () => {
     try {
       // STEP 1: Replace this URL with your Google Apps Script Web App URL
       // Follow the instructions in google-sheets-setup.md to get your URL
-      const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwqGnCZ9hUrkWfUoQEcMe9HKhKz5GX_JMPEHc5U1SDo1lQRH_lo7HmwxjOwVWlPS8X8/exec';
+      const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycby1QoNIbkLh1JFO9xz1Xg6WoKEkGJ18Q2PaA0X7xISsGXQHQU67b9JzpmYHdXPU_LGr/exec';
       
       const response = await fetch(GOOGLE_SCRIPT_URL, {
         method: 'POST',
